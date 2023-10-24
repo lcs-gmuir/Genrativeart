20903329b50ee681736d226edd644f8efe780864
Griffin Muir <gmuir@lcs.on.ca>
2023-10-24 11:45:13 -0400

    made new views folder moved everything to the folder and made a new view file called title view

---
+++ Genrativeart/views/TitleView.swift
@@ -0,0 +1,20 @@
+//
+//  TitleView.swift
+//  Genrativeart
+//
+//  Created by Griffin Muir on 2023-10-24.
+//
+
+import SwiftUI
+
+struct TitleView: View {
+    var body: some View {
+        Rectangle()
+        .aspectRatio(1.0, contentMode: .fit)
+        .foregroundColor(.white)
+    }
+}
+
+#Preview {
+    TitleView()
+}
