fedde0ad6b737406044fc2cf7dbd8f3ddec357aa
Griffin Muir <gmuir@lcs.on.ca>
2023-10-24 11:30:13 -0400

    Initial Commit

---
+++ Genrativeart/GenrativeartApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenrativeartApp.swift
+//  Genrativeart
+//
+//  Created by Griffin Muir on 2023-10-24.
+//
+
+import SwiftUI
+
+@main
+struct GenrativeartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
