c214f7393089d1ad6c0b3b697a19d278976c6551
Griffin Muir <gmuir@lcs.on.ca>
2023-10-28 10:10:06 -0400

    replaced the old loops with another singular loop to try and cut down on usless repetive code

--- Genrativeart/views/gridView.swift
+++ Genrativeart/views/gridView.swift
@@ -10,23 +10,17 @@ import SwiftUI
 struct gridView: View {
     var body: some View {
 Grid(horizontalSpacing: 0, verticalSpacing: 0){
-            GridRow {
-                ForEach(1..<4){i in
-                    TitleView()
-                }
-             
-                
+    
+    
+    ForEach(1..<4) { j in
+        GridRow {
+            ForEach(1..<4){i in
+                TitleView()
             }
-    GridRow{
-        ForEach(1..<4){i in
-            TitleView()
         }
     }
-    GridRow {
-        ForEach(1..<4){i in
-            TitleView()
-        }
-    }
+
+  
         }
     }
 }
