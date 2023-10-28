2f2c298b2fe950d3416b5f170b8376c98b187cf3
Griffin Muir <gmuir@lcs.on.ca>
2023-10-28 10:37:00 -0400

    added sliders changed the columns and rows to doubles to function within our sliders also convereted the doubles to ints so that they functioned within the loop

--- Genrativeart/views/gridView.swift
+++ Genrativeart/views/gridView.swift
@@ -9,15 +9,14 @@ import SwiftUI
 
 struct gridView: View {
     @State var rows = 4.0
-    @State var columns = 4
-    
+    @State var columns = 4.0
 
     var body: some View {
 Grid(horizontalSpacing: 0, verticalSpacing: 0){
     
-    ForEach(0..<rows, id: \.self) { j in
+    ForEach(0..<Int(rows), id: \.self) { j in
         GridRow {
-            ForEach(0..<columns, id: \.self){i in
+            ForEach(0..<Int(columns), id: \.self){i in
                 TitleView()
             }
         }
@@ -25,8 +24,14 @@ Grid(horizontalSpacing: 0, verticalSpacing: 0){
 
   
         }
+        Text("\(rows)")
+
 Slider(value: $columns, in: 0...100, step: 1.0)
         
+        
+Slider(value: $rows, in: 0...100, step: 1.0)
+        Text("\(columns)")
+
     }
 }
 
